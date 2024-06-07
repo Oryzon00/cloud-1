4e9c4cc46d6ca98e5fd895f242b7994e3ac09cd3
Adrian <adrian.mael.jung@gmail.com>
2024-06-07 19:18:09 +0200

    WIP make wordpress works

--- requirements/wordpress/script/config.sh
+++ requirements/wordpress/script/config.sh
@@ -1,35 +1,53 @@
-# if [ ! -f "$PROTECT_FILE" ]; then
-		rm -f /var/www/wordpress/wp-config.php
+echo "start script "
 
-		wp config create	--allow-root \
-							--dbname=$MYSQL_DATABASE \
-							--dbuser=$MYSQL_USER \
-							--dbpass=$MYSQL_PASSWORD \
-							--dbhost=mariadb:3306 \
-							--path='/var/www/wordpress' \
-							--skip-check
+# download wordpress
+wp core download --allow-root
 
-		wp core install 	--url="cloudaj.duckdns.org" \
-							--title="My website" \
-							--admin_user=$WP_ADMIN_USER \
-							--admin_password=$WP_ADMIN_PASSWORD \
-							--admin_email=$WP_ADMIN_EMAIL \
-							--skip-email \
-							--path="/var/www/wordpress/" \
-							--allow-root
+sleep 5
 
-		wp user create	$WP_AUTHOR_USER $WP_AUTHOR_EMAIL \
-							--role=author	\
-							--user_pass=$WP_AUTHOR_PASSWORD	\
-							--allow-root	\
-							--path='/var/www/wordpress'
+if [ -f /var/www/html/wp-config.php ]
+then
+	echo "===> wp-config.php already exist <==="
+else
+	echo "in condition"
 
-# 		touch $PROTECT_FILE
-# fi
+	wp config create	--allow-root \
+						--dbname=$MYSQL_DATABASE \
+						--dbuser=$MYSQL_USER \
+						--dbpass=$MYSQL_PASSWORD \
+						--dbhost=mariadb:3306 \
+						--path='/var/www/html' \
+						--skip-check
 
-# #Check if /run/php exist to prevent an error from php
-# if ! [ -d "/run/php" ]; then
-#     mkdir /run/php
-# fi
+sleep 5
 
-# exec php-fpm7.3 --nodaemonize
+	wp core install 	--url="cloudaj.duckdns.org" \
+						--title="My website" \
+						--admin_user=$WP_ADMIN_USER \
+						--admin_password=$WP_ADMIN_PASSWORD \
+						--admin_email=$WP_ADMIN_EMAIL \
+						--skip-email \
+						--path="/var/www/html/" \
+						--allow-root
+
+sleep 5
+
+	wp user create	$WP_AUTHOR_USER $WP_AUTHOR_EMAIL \
+						--role=author	\
+						--user_pass=$WP_AUTHOR_PASSWORD	\
+						--allow-root	\
+						--path='/var/www/html'
+fi
+
+#Check if /run/php exist to prevent an error from php
+if ! [ -d "/run/php" ]; then
+    mkdir /run/php
+fi
+
+chown -R www-data:www-data /var/www/*
+chmod -R 755 /var/www/*
+
+sleep 2
+echo "===> WORDPRESS IS UP <==="
+
+exec php-fpm7.3 --nodaemonize
