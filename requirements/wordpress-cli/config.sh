ba52485806ad7ed8c77812debd3fc4b33a3f2b52
Adrian <adrian.mael.jung@gmail.com>
2024-06-08 19:08:44 +0200

    [UP] Config wordpress thrugh wordpress-cli image

---
+++ requirements/wordpress-cli/config.sh
@@ -0,0 +1,34 @@
+PROTECT_FILE="/var/www/html/.wordpress_protect"
+
+# sleep 20
+
+if  [ ! -f "$PROTECT_FILE" ]; then
+
+	echo "Wordpress is not configured, configuring..."
+
+	rm -f	/var/www/html/wp-config.php
+
+	wp config create	--allow-root \
+						--dbname=$MYSQL_DATABASE \
+						--dbuser=$MYSQL_USER \
+						--dbpass=$MYSQL_PASSWORD \
+						--dbhost=mariadb \
+						--path='/var/www/html' \
+						--skip-check
+
+	wp core install 	--url="cloudaj.duckdns.org" \
+						--title="My website" \
+						--admin_user=$WP_ADMIN_USER \
+						--admin_password=$WP_ADMIN_PASSWORD \
+						--admin_email=$WP_ADMIN_EMAIL \
+						--skip-email \
+						--path="/var/www/html" \
+						--allow-root
+	
+	touch $PROTECT_FILE
+
+echo "Wordpress configured !"
+
+fi
+
+echo "Wordpress is ready !"
